b8acdbacee518932de544b914a7ac6e43a661174
Pamela Coleman <colemanp3@duvalschools.org>
2025-04-26 17:18:28 -0400

    Update CalendlyEmbed.tsx
    Reusable calendly

--- src/components/calendly/CalendlyEmbed.tsx
+++ src/components/calendly/CalendlyEmbed.tsx
@@ -34,3 +34,29 @@ const CalendlyEmbed: React.FC<CalendlyEmbedProps> = ({
 };
 
 export default CalendlyEmbed;
+import React from "react";
+
+interface CalendlyEmbedProps {
+  url?: string;
+  className?: string;
+}
+
+const CalendlyEmbed: React.FC<CalendlyEmbedProps> = ({
+  url = "https://calendly.com/gP82w-LylhBf7GzVW7Z24xu4AKcKBpPhhT0x82pi3gw",
+  className = "custom-class",
+}) => {
+  return (
+    <div className={className}>
+      <iframe
+        src={url}
+        frameBorder="0"
+        width="100%"
+        height="600px"
+        style={{ border: "none" }}
+        title="Calendly Embed"
+      />
+    </div>
+  );
+};
+
+export default CalendlyEmbed;
